76be44fdf39a25caa0ecf84118b30b2d63bfc34a
廖焌邑 <13235235+liao-yanyi@user.noreply.gitee.com>
2023-07-28 15:21:35 +0800

    add new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,35 @@
-import { Button, Input, Space, Spin } from "antd";
-import React, { useEffect, useState } from "react";
-
-const { Search } = Input;
+import { Divider } from "antd";
+import { SearchOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import "./App.css";
 
-const ComponentOne = (props: { isShow: boolean }) => {
-  const { isShow } = props;
+const App = () => (
+  <>
+    <header className="wexin">
+      <div className="left">微信</div>
+
+      <div className="right">
+        <SearchOutlined />
+        <PlusCircleOutlined />
+      </div>
+    </header>
+    <Chat />
+  </>
+);
+const Chat = () => {
   return (
-    <div style={{ marginTop: 100 }}>
-      {isShow ? <Spin /> : <div>not show</div>}
+    <div className="Chat">
+      <Divider className="Divide" />
+      <img
+        src="https://picx.zhimg.com/80/v2-6afa72220d29f045c15217aa6b275808_720w.webp?source=1940ef5c"
+        width="32"
+        height="32"
+      />
+      <div className="content">
+        <p>用户名</p>
+        <p>聊天内容</p>
+        <Divider />
+      </div>
     </div>
   );
 };
-
-const App: React.FC = () => {
-  const [value, setValue] = useState<undefined | string>();
-  const [isShow, setIsShow] = useState<boolean>(false);
-
-  useEffect(() => {
-    console.log("componentDidMount");
-  }, []);
-  const onSearch = (value: string) => {
-    console.log(value);
-    setValue(value);
-  };
-  return (
-    <>
-      <div style={{ marginTop: "-300px" }}>
-        <div>
-          <img
-            src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
-            width="270"
-            height="129"
-          />
-        </div>
-        <div>
-          {" "}
-          <Space>
-            <Search
-              style={{ width: 550 }}
-              value={value}
-              allowClear
-              enterButton="百度一下"
-              size="large"
-              onSearch={onSearch}
-            />
-          </Space>
-        </div>
-        <Button
-          onClick={() => {
-            setIsShow((v) => !v);
-          }}
-        >
-          switch isShow
-        </Button>
-        <ComponentOne isShow={isShow} />
-      </div>
-    </>
-  );
-};
-
 export default App;
