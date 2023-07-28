e7fab07215ceeb214fc585860ed0759e6a8f7eef
lighthx <770914qq>
2023-07-28 16:09:09 +0800

    feat: add init chat item

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { Divider } from "antd";
 import { SearchOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import "./App.css";
 
+const list = [1, 2, 3, 4, 5, 6];
+
 const App = () => (
   <>
-    <header className="wexin">
-      <div className="left">微信</div>
-
+    <div className="wexin">
+      <div className={"left"} />
+      <div className="center">微信</div>
       <div className="right">
-        <SearchOutlined />
-        <PlusCircleOutlined />
+        <SearchOutlined className={"icon"} />
+        <PlusCircleOutlined className={"icon"} />
       </div>
-    </header>
-    <Chat />
+    </div>
+    {list.map((item) => (
+      <Chat key={item} />
+    ))}
   </>
 );
 const Chat = () => {
   return (
-    <div className="Chat">
-      <Divider className="Divide" />
-      <img
-        src="https://picx.zhimg.com/80/v2-6afa72220d29f045c15217aa6b275808_720w.webp?source=1940ef5c"
-        width="32"
-        height="32"
-      />
-      <div className="content">
-        <p>用户名</p>
-        <p>聊天内容</p>
-        <Divider />
+    <div className="chatItem">
+      <div className={"chatAvatarView"}>
+        <img
+          src="https://picx.zhimg.com/80/v2-6afa72220d29f045c15217aa6b275808_720w.webp?source=1940ef5c"
+          className={"avatar"}
+        />
       </div>
+      <div className="chatContent">
+        <div className={"username"}>用户名</div>
+        <div className={"comment"}>聊天内容</div>
+      </div>
+      <div className={"dateView"}>1:30</div>
     </div>
   );
 };
