6f3c62e207d6a67d3e815760f72aec3407df1b1c
xin <lighthx@gmail.com>
2023-09-16 14:56:42 +0800

    fix: scroll style

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,48 @@ import {
 } from "@ant-design/icons";
 import { Carousel } from "antd";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CarouselRef } from "antd/es/carousel";
+
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const App = () => {
   const navigate = useNavigate();
-  const [value, setValue] = useState(1);
+  const ref = useRef<CarouselRef>(null);
+  const [viewIndex, setViewIndex] = useState(0);
   const [transitionStage, setTransistionStage] = useState("fadeIn");
+  const contentHeight = window.screen.height - 164;
   useEffect(() => {
     setTransistionStage("fadeIn");
   }, []);
   return (
-    <>
-      <div>
-        <header className="fixed w-full text-xl flex items-center justify-between bg-neutral-100 h-16 box-border">
-          <div className="flex flex-1 box-border" />
-          <div className="flex flex-1 justify-center items-center box-border">
-            微信
-          </div>
-          <div className="flex flex-1 justify-end items-center box-border">
-            <SearchOutlined className={"text-xl mr-5 box-border"} />
-            <PlusCircleOutlined className={"text-xl mr-5 box-border"} />
-          </div>
-        </header>
-
-        <div className="h-16"></div>
-        <Carousel>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <header
+        className="w-full text-xl flex items-center justify-between bg-neutral-100 box-border"
+        style={{ height: "100px" }}
+      >
+        <div className="flex flex-1 box-border" />
+        <div className="flex flex-1 justify-center items-center box-border">
+          微信
+        </div>
+        <div className="flex flex-1 justify-end items-center box-border">
+          <SearchOutlined className={"text-xl mr-5 box-border"} />
+          <PlusCircleOutlined className={"text-xl mr-5 box-border"} />
+        </div>
+      </header>
+      <div style={{ overflow: "auto", height: contentHeight }}>
+        <Carousel
+          dots={false}
+          ref={ref}
+          afterChange={(v) => {
+            setViewIndex(v);
+          }}
+        >
           <div
             className={`${transitionStage}`}
             onClick={() => {
@@ -60,12 +76,18 @@ const App = () => {
           </div>
           <div>4</div>
         </Carousel>
-        <div className="h-16"></div>
-        <div className="h-5 fixed">
-          <Underside />
-        </div>
       </div>
-    </>
+      <Underside
+        index={viewIndex}
+        setViewIndex={(i) => {
+          setViewIndex(i);
+          if (ref.current == null) {
+            return;
+          }
+          ref.current.goTo(i);
+        }}
+      />
+    </div>
   );
 };
 export const Chat = () => {
@@ -90,27 +112,40 @@ export const Chat = () => {
   );
 };
 
-const Underside = () => {
+const tabs = [
+  { name: "微信", icon: <MessageOutlined /> },
+  { name: "通讯录", icon: <TeamOutlined /> },
+  { name: "发现", icon: <CompassOutlined /> },
+  { name: "我", icon: <UserOutlined /> },
+];
+
+const Underside = ({
+  setViewIndex,
+  index,
+}: {
+  setViewIndex: (v: number) => void;
+  index: number;
+}) => {
   return (
-    <>
-      <div className="bottom-0 fixed w-full text-xl bg-neutral-100 content-center flex box-border h-16 items-center">
-        <div className="flex-col justify-center flex flex-1 box-border items-center">
-          <MessageOutlined />
-          微信
+    <div
+      className="bottom-0 w-full text-xl bg-neutral-100 content-center flex box-border items-center"
+      style={{ height: "64px" }}
+    >
+      {tabs.map((tab, i) => (
+        <div
+          className={`flex-col justify-center flex flex-1 box-border items-center ${
+            i === index ? "text-blue-600/75" : ""
+          }`}
+          key={tab.name}
+          onClick={() => {
+            setViewIndex(i);
+          }}
+        >
+          {tab.icon}
+          {tab.name}
         </div>
-        <div className="flex-col justify-center flex flex-1 box-border items-center">
-          <TeamOutlined />
-          通讯录
-        </div>
-        <div className="flex-col justify-center flex flex-1 box-border items-center">
-          <CompassOutlined />
-          发现
-        </div>
-        <div className="flex-col justify-center flex flex-1 box-border items-center">
-          <UserOutlined />我
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
